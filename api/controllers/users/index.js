8e5bd8c5a3856820edb3cbabd4837bb6037dfef3
Antonio Fernandez Paternina <info@antoniofernandez.pro>
2020-07-28 19:22:42 -0500

    validacion de username y nauk

--- api/controllers/users/index.js
+++ api/controllers/users/index.js
@@ -9,6 +9,9 @@ const { response } = require('express');
 const crypto = require('./../../functions/crypto');
 const config = require('./../../../config');
 const validatePassword = require('./../../functions/validatePassword');
+const { promises } = require('fs');
+const { resolve } = require('path');
+const { rejects } = require('assert');
 
 /*
 const getAll = (req, res) =>{
@@ -21,6 +24,50 @@ const getAll = (req, res) =>{
     })
 };*/
 
+
+const validateUser = (username, email) => {
+    return new Promise((resolve, reject) => {
+        r = true;
+        User.findOne({username: username})
+        .then(response => {
+        if(response.username){
+            r = false;
+            console.log("Usersame: "+response.username+" "+r);
+            response.sendStatus(500).send("Datos invalidos.");
+        }
+        }).catch((err)=>{
+            reject(new Error(err));
+            response.sendStatus(500);
+        });
+        User.findOne({email: email})
+        .then(responseMail => {
+            if(responseMail.email){
+                r = false;
+                console.log("email: "+responseMail.email +" "+r);
+                responseMail.sendStatus(500).send("Datos invalidos.");
+            }
+        })
+        .catch((err)=>{
+            reject(new Error(err));
+            responseMail.sendStatus(500);
+        });
+        setTimeout(() => {
+            console.log("Resultado funcion async: "+r);
+            resolve(r);
+        },1500);
+    });
+}
+
+async function validateUserAsync(username, email){
+    try{
+        let valida = await validateUser(username, email);
+        console.log("Valida: "+valida);
+        console.log("tipo: "+typeof(valida));
+        return valida;
+    } catch(err){
+        console.log(err);
+    }
+}
 
 const getUsers = (req, res) => {
     res.sendStatus(200);
@@ -46,11 +93,54 @@ const newUser = (req, res) => {
     const salt = bcrypt.genSaltSync(saltRounds);
     const password = bcrypt.hashSync(req.body.password, salt);
 
-    const birthdate = crypto.encrypt(req.body.birthdate);  
-
+    const birthdate = crypto.encrypt(req.body.birthdate);
+    
     if (!validatePassword(req.body.password)){
+        console.log(validatePassword(req.body.password));
         res.sendStatus(500).send("Contraseña invalida.");
     }
+
+    let r = true;
+    
+    /*User.findOne({username: req.body.username})
+        .then(response => {
+        if(response.username){
+            r = false;
+            console.log("Usersame: "+response.username+" "+r);
+            console.log("Invoca a la funcion: "+r);
+            res.sendStatus(500).send("Datos invalidos.");
+        }
+        }).catch((err)=>{
+            //reject(new Error(err));
+            res.sendStatus(500).send(err);
+        });
+
+    User.findOne({email: req.body.email})
+        .then(response => {
+            if(response.email){
+                r = false;
+                console.log("email: "+response.email +" "+r);
+                console.log("Invoca a la funcion: "+r);
+                res.sendStatus(500).send("Datos invalidos.");
+            }
+        }).catch((err)=>{
+            //reject(new Error(err));
+            res.sendStatus(500).send(err);
+        });*/
+
+    //let r = validateUser(req.body.username, req.body.email);
+    //console.log(r);S
+    //let r1 = validateUser(req.body.username, req.body.email)
+      //      .then(r => console.log(r));
+    r = validateUserAsync(req.body.username, req.body.email);
+    console.log(r);
+    console.log(typeof(r));
+    //console.log(r1.then(r => console.log(r)));
+    //console.log("Validacion de usuario: "+r1.then(r => resolve(r)));
+    /*if (!r){
+        console.log("Invoca a la funcion: "+r);
+        res.sendStatus(500).send("Datos invalidos.");
+    }*/
 
     const user = {
         name: req.body.name,
@@ -66,7 +156,7 @@ const newUser = (req, res) => {
         birthdate: birthdate
     };
 
-    if(user.name && user.age && user.username && user.password && user.email){
+    if(user.name && user.age && user.username && user.password && user.email && r){
         const object = new User(user);
         object.save()
         .then((response)=>{
