3ebaaff2d91b3fbcc4f4d60bd8566b342217ec86
Juan Carlos Peña Peña <penapena@uninorte.edu.co>
2020-07-31 18:06:36 -0500

    se corrige ruta de carpeta de logs

--- api/middleware/logger/index.js
+++ api/middleware/logger/index.js
@@ -6,7 +6,7 @@ const logger = (req, res, next) => {
     const parameters = JSON.stringify(req.params);
     const ua = JSON.stringify(req.useragent);
     const log = `${date()} :: ${req.method} :: ${req.path} : ${parameters} : ${ua}`;
-    const folder = config.server.logs.dir | 'logs';
+    const folder = config.server.logs.dir || 'logs';
     fs.appendFile(`${folder}/logger.log`, log, 'utf8', (err)=>{
         if (err)
             console.log(err);
